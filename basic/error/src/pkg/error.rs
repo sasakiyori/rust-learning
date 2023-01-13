84f3b63dd467d44748b1a32d57c307933d4c3b7a
sasakiyori <sasakiyori33@gmail.com>
2023-01-13 16:22:58 +0800

    cargo fmt

--- basic/error/src/pkg/error.rs
+++ basic/error/src/pkg/error.rs
@@ -1,9 +1,12 @@
-pub fn trouble_maker(i : i32) -> Result<i32, bool> {
-    if i >= 0 { Ok(i) }
-    else { Err(false) }
+pub fn trouble_maker(i: i32) -> Result<i32, bool> {
+    if i >= 0 {
+        Ok(i)
+    } else {
+        Err(false)
+    }
 }
 
-pub fn error_processor(res : Result<i32, bool>) -> Result<i32, bool> {
+pub fn error_processor(res: Result<i32, bool>) -> Result<i32, bool> {
     let x = res?;
     Ok(x)
 }
--- basic/error/src/pkg/mod.rs
+++ basic/error/src/pkg/mod.rs
@@ -1,7 +1,6 @@
 pub mod error;
 
-
-pub fn error_processor(res : Result<i32, bool>) -> Result<i32, bool> {
+pub fn error_processor(res: Result<i32, bool>) -> Result<i32, bool> {
     let x = res?;
     Ok(x)
 }
