dfdc149c7b186e529290c28c416b252441803e9d
sasakiyori <sasakiyori33@gmail.com>
2023-04-12 17:06:07 +0800

    improve by iterator

--- program/minigrep/src/main.rs
+++ program/minigrep/src/main.rs
@@ -2,8 +2,16 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
+    /*
     let args: Vec<String> = env::args().collect();
     let cfg = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    */
+
+    // improve:
+    let cfg = Config::build(env::args()).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
