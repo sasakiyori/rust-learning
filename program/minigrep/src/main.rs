4426893474b23979fb8403ce88af71b8f63665e1
sasakiyori <sasakiyori33@gmail.com>
2023-04-12 11:48:25 +0800

    add minigrep

---
+++ program/minigrep/src/main.rs
@@ -0,0 +1,15 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let cfg = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(cfg) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
